a1d7e0f98a09799efa6517c7046e6b8c3f1c9009
Seemant-Singh <68770067+Seemant-Singh@users.noreply.github.com>
2021-11-06 09:26:24 +0530

    Add files via upload

--- Rev.java
+++ Rev.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 public class Rev{
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int[] arr={1,2,3,4,5,6,7};
-        int target=4;
-        System.out.println(Arrays.toString(linearsearch(arr,target)));
+        // int[] arr={1,2,3,4,5,6,7};
+        String str="kunal";
+        char target='u';
+        System.out.println(linearsearch(str,target));
     }
-    static int[] linearsearch(int[] arr,int target){
-        if(arr.length==0)
-            return new int[]{-1};
-        // for(int i=0;i<arr.length;i++)
-        // {
-        //     if(target==arr[i])
-        //         return new int[]{i};
-        // }
-        for(int element:arr)
+    static boolean linearsearch(String str,char target){
+        if(str.length()==0)
+            return false;
+ 
+        for(char element:str.toCharArray())
         {
             if(target==element)
-                return new int[]{element};
+                return true;
         }
-        return new int[]{-1};
+        return false;
     }
 }
