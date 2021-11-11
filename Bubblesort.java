442d9b578aae560bb3b8dacdcffc5638a4835bdc
Seemant-Singh <68770067+Seemant-Singh@users.noreply.github.com>
2021-11-11 12:03:42 +0530

    Add files via upload

--- Bubblesort.java
+++ Bubblesort.java
@@ -2,13 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class bubblesort{
     public static void main(String[] args){
-        int[] arr={1,2,5,4,3};
+        int[] arr={1,2,3,4,5};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void bubble(int[] arr){
         for(int i=0;i<arr.length-1;i++)
         {
+            boolean swapped=false;
             for(int j=1;j<arr.length-i;j++)
             {
                 if(arr[j]<arr[j-1])
@@ -16,8 +17,11 @@ public class bubblesort{
                     int temp=arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
+                    swapped=true;
                 }
             }
+            if(!swapped)
+                break;
         }
     }
 }
