fa4b0f7b7c873e78fa626972038e3d47266404f5
Seemant-Singh <68770067+Seemant-Singh@users.noreply.github.com>
2021-11-24 11:41:37 +0530

    Add files via upload

---
+++ Maxdiff.java
@@ -0,0 +1,16 @@
+public class  Maxdiff {
+    public static void main(String[] args) {
+        int[] arr={2,3,10,6,4,8,1};
+        System.out.println(maxDiff(arr));
+    }
+    static int maxDiff(int[] arr){
+        int n=arr.length;
+        int res=arr[1]-arr[0];
+        int minVal=arr[0];
+        for(int i=1;i<arr.length;i++){
+            res=Math.max(res,arr[i]-minVal);
+            minVal=Math.min(minVal,arr[i]);
+        }
+        return res;
+    }
+}
